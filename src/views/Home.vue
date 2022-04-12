c0136fe152845111d91af9b4b9d405f24d260432
Lucas Pina <ti.lucaspina@gmail.com>
2022-04-12 09:03:18 -0300

    contiuação do projeto
    home, lista produtos, axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,28 @@
 <template>
-  <div></div>
+  <section>
+    <h1>👍 Compre ou Venda 👎</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+
 export default {
-  name: "Home",
+  name: "HomePage",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  },
 };
 </script>
+
+<style>
+h1 {
+  color: #87f;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>

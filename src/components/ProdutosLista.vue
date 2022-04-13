e606ae893ec6c074397b8e9743d601c35e3b6360
Lucas Pina <ti.lucaspina@gmail.com>
2022-04-13 08:43:58 -0300

    Buscar, estilo de produtos

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,31 +1,53 @@
 <template>
   <section class="produtos-container">
-    <div v-for="produto in produtos" :key="produto.id">
-      <img
-        v-if="produto.fotos"
-        :src="produto.fotos[0].src"
-        :alt="produto.fotos[0]"
-      />
-      <p>{{ produto.descricao }}</p>
-      <h2 class="titulo">{{ produto.nome }}</h2>
-      <p class="preco">{{ produto.preco }}</p>
+    <div v-if="produtos && produtos.length" class="produtos">
+      <div class="produto" v-for="produto in produtos" :key="produto.id">
+        <router-link to="/">
+          <img
+            v-if="produto.fotos"
+            :src="produto.fotos[0].src"
+            :alt="produto.fotos[0]"
+          />
+          <p>{{ produto.descricao }}</p>
+          <h2 class="titulo">{{ produto.nome }}</h2>
+          <p class="preco">{{ produto.preco }}</p>
+        </router-link>
+      </div>
+    </div>
+    <div v-else-if="produtos && produtos.length === 0">
+      <p class="sem-resultados">
+        Busca sem resultados. Tente buscar outro termo
+      </p>
     </div>
   </section>
 </template>
 
 <script>
 import { api } from "@/services/services";
+import { serialize } from "@/helpers/helpers";
 export default {
   data() {
     return {
       produtos: null,
+      produtosPorPagina: 9,
     };
+  },
+  computed: {
+    url() {
+      const query = serialize(this.$route.query);
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
+    },
   },
   methods: {
     getProdutos() {
-      api.get("/produto").then((response) => {
+      api.get(this.url).then((response) => {
         this.produtos = response.data;
       });
+    },
+  },
+  watch: {
+    url() {
+      this.getProdutos();
     },
   },
   created() {
@@ -34,4 +56,48 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px;
+}
+
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.produto:hover {
+  box-shadow: 0 6px 8px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position: relative;
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+
+.sem-resultados {
+  text-align: center;
+}
+</style>

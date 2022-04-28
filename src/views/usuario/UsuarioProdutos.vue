22f59894e682ce207163370deb616561919ee333
Lucas Pina <ti.lucaspina@gmail.com>
2022-04-28 08:58:17 -0300

    Usuário produtos, Produto Adicionar, Produto Deletar

--- src/views/usuario/UsuarioProdutos.vue
+++ src/views/usuario/UsuarioProdutos.vue
@@ -1,9 +1,89 @@
 <template>
-  <p>Lista de Produtos</p>
+  <section>
+    <h2>Adicionar produto</h2>
+    <ProdutoAdicionar />
+    <h2>Seus Produtos</h2>
+    <transition-group v-if="usuario_produtos" name="list" tag="ul">
+      <li v-for="(produto, index) in usuario_produtos" :key="index">
+        <ProdutoItem :produto="produto">
+          <p>{{ produto.descricao }}</p>
+          <button class="deletar" @click="deletarProduto"></button>
+        </ProdutoItem>
+      </li>
+    </transition-group>
+  </section>
 </template>
 
 <script>
-export default {};
+import ProdutoAdicionar from "@/components/ProdutoAdicionar";
+import ProdutoItem from "@/components/ProdutoItem";
+import { mapState, mapActions } from "vuex";
+import { api } from "@/services/services";
+
+export default {
+  name: "UsuarioProdutos",
+  components: {
+    ProdutoAdicionar,
+    ProdutoItem,
+  },
+  computed: {
+    ...mapState(["login", "usuario", "usuario_produtos"]),
+  },
+  methods: {
+    ...mapActions(["getUsuarioProdutos"]),
+    deletar(id) {
+      const confirmar = window.confirm("Deseja remover este produto?");
+      if (confirmar) {
+        api
+          .delete(`/produtos/${id}`)
+          .then(() => {
+            this.getUsuarioProdutos();
+          })
+          .catch((error) => {
+            console.log(error.response);
+          });
+      }
+    },
+  },
+  watch: {
+    login() {
+      this.getUsuarioProdutos();
+    },
+  },
+  created() {
+    if (this.login) {
+      this.getUsuarioProdutos();
+    }
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+}
+
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translated3d(20px, 0, 0);
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.3s;
+}
+
+.deletar {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  background: url("../../assets/remove.svg") no-repeat center center;
+  width: 24px;
+  height: 24px;
+  text-indent: -140px;
+  overflow: hidden;
+  cursor: pointer;
+  border: none;
+}
+</style>
